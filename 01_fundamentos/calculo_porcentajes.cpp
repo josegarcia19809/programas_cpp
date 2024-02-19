ce773afd02f426cc3c64e97afe2e8e538e2cb02b
josegarcia19809 <josegarcia19809@gmail.com>
2024-02-18 18:47:08 -0600

    Cálculo de porcentajes

---
+++ 01_fundamentos/calculo_porcentajes.cpp
@@ -0,0 +1,26 @@
+// Este programa pide un número y calcula su 5%, 10% y 15%
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numero;
+    double porcentaje1, porcentaje2, porcentaje3;
+
+    cout << endl;
+    cout << "Programa para calcular el 5%, 10% y 15% de un número" << endl;
+    cout << "Dame un número: ";
+    cin >> numero;
+
+    porcentaje1 = numero * 5 / 100.0;
+    porcentaje2 = numero * 10 / 100.0;
+    porcentaje3 = numero * 15 / 100.0;
+
+    cout << "Número original: " << numero << endl;
+    cout << "Su 5% es: " << porcentaje1 << endl;
+    cout << "Su 10% es: " << porcentaje2 << endl;
+    cout << "Su 15% es: " << porcentaje3 << endl;
+
+
+    return 0;
+}
